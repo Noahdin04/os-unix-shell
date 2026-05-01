e57ccab2b4e80f60b971b265fe665e92fc7f4d1a
ItsMeChef <JenkinsJ2703@gmail.com>
2026-05-01 16:52:19 -0500

    input + output redirection
    also added comments for clear implementation details

--- myshell.c
+++ myshell.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 
 #define MAX_INPUT 1024
 #define MAX_ARGS 64
@@ -58,6 +59,10 @@ int main(void)
                         printf("Shell terminated.\n");
                         break;
                 }
+
+
+                /* -=-=-=-=-=-=-= background detection -=-=-=-=-=-=-= */
+
                 /* check for trailing & before parsing */
                 int is_background = 0;
                 int i = strlen(input);
@@ -74,20 +79,91 @@ int main(void)
                                 input[--i] = '\0';
                         }
                 }
+
+
+                /* -=-=-=-=-=-=-= parse command -=-=-=-=-=-=-= */
+
                 /* TODO: parse_command(input, argv, MAX_ARGS); if no args, continue */
                 if (parse_command(input, argv, MAX_ARGS) == 0) {
                         continue;
                 }
+
+                /* -=-=-=-=-=-=-= Redirection Parsing -=-=-=-=-=-=-= */
+
+                char *in_file = NULL;
+                char *out_file = NULL;
+        
+                char *clean_argv[MAX_ARGS];
+                int j = 0;
+        
+                for (int i = 0; argv[i] != NULL; i++) {
+        
+                    if (strcmp(argv[i], "<") == 0) {
+                        if (argv[i + 1] == NULL) {
+                            printf("Error,  requires a filename.\n");
+                            continue;
+                        }
+                        in_file = argv[i + 1];
+                        i++; 
+                    }
+                    else if (strcmp(argv[i], ">") == 0) {
+                        if (argv[i + 1] == NULL) {
+                            printf("Error, requires a filename.\n");
+                            continue;
+                        }
+                        out_file = argv[i + 1];
+                        i++; 
+                    }
+                    else {
+                        clean_argv[j++] = argv[i];
+                    }
+                }
+        
+                clean_argv[j] = NULL;
+        
+                if (clean_argv[0] == NULL) {
+                    continue;
+                }
+
+                /* -=-=-=-=-=-=-= End of Redirection Parsing -=-=-=-=-=-=-= */
+
+                
                 pid_t pid = fork();
+
                 if (pid < 0) {
                         perror("fork");
                         continue;
                 }
                 if (pid == 0) {
                         /* TODO: Child: execute command */
-                        execvp(argv[0], argv);
-                        perror("execvp");
-                        exit(1);
+
+                        /* -=-=-=-=-=-=-= Input Redirection -=-=-=-=-=-=-= */
+                        
+                        if (in_file != NULL) {
+                                int fd = open(in_file, O_RDONLY);
+                                if (fd < 0) {
+                                    perror("error opening input file");
+                                    exit(1);
+                                }
+                                dup2(fd, STDIN_FILENO);
+                                close(fd);
+                            }
+                
+                            /* -=-=-=-=-=-=-= Output Redirection -=-=-=-=-=-=-= */
+
+                            if (out_file != NULL) {
+                                int fd = open(out_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                                if (fd < 0) {
+                                    perror("error opening output file");
+                                    exit(1);
+                                }
+                                dup2(fd, STDOUT_FILENO);
+                                close(fd);
+                            }
+                
+                            execvp(clean_argv[0], clean_argv);
+                            perror("execvp");
+                            exit(1);
                 /* TODO: If execvp returns, it failed */
                 } else {
                         if (is_background) {
